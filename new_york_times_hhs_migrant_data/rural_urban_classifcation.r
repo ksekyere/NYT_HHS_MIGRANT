bfad81ad2b0f927d8ec08d97af197bfb15be7b0f
Kofi Sekyere <58433475+ksekyere@users.noreply.github.com>
2025-05-05 19:35:01 +0000

    factoring & labeling ruca codes

--- new_york_times_hhs_migrant_data/rural_urban_classifcation.r
+++ new_york_times_hhs_migrant_data/rural_urban_classifcation.r
@@ -6,19 +6,38 @@ library(ggplot2)
 sponsor_geo_info <- left_join(
     days_till_reunification,
     geo_info,
-    by = c("Sponsor.Zipcode" = "ZIP_CODE")) %>%
-    rename(zip_code = "Sponsor.Zipcode")  %>%
-    rename_with(str_to_lower)
+    by = c("Sponsor.Zipcode" = "ZIP_CODE")
+) %>%
+    rename(zip_code = "Sponsor.Zipcode") %>%
+    rename_with(str_to_lower) %>%
+    mutate(
+        ruca1 = factor(
+            ruca1,
+            levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "99", "NA"),
+            labels = c(
+                "Metropolitan area core",
+                "Metropolitan area high commuting",
+                "Metropolitan area low commuting",
+                "Micropolitan area core",
+                "Micropolitan high commuting",
+                "Micropolitan low commuting",
+                "Small town core",
+                "Small town high commuting",
+                "Small town low commuting",
+                "Rural areas",
+                "Not coded"
+            )
+        )
+    )
 
 
 
-names(sponsor_geo_info)
 
 #breakdown of released by rural/urban classification
 sponsor_geo_info %>%
     group_by(pop_cat, release_month) %>%
     #filter out the < 250 entries that do not have legit zip codes
-    filter(!is.na(pop_cat)) %>%
+    #filter(!is.na(pop_cat)) %>%
     group_by(ruca1, release_month) %>%
     #calculate total releases for each zcta
     summarise(total_kids = n()) %>%
